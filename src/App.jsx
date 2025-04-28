773746e035f92109a4eac6edcaa06be4e92b4ebb
Yato <darkflamesmage23@gmail.com>
2025-04-27 21:37:13 -0300

    Se Agrega la estructura en app y TaskForm por Enrique Real

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TaskList from './taskList'
+import Taskform from './Taskform'
+import {task as data} from "./tasks"
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+  
+  function createtask (task) {
+    setTasks(...tasks, {title: task.title, description: task.description, id: tasks.length + 1});
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <taskform createtask={createtask}/>
+      <TaskList/>
     </>
-  )
-}
-
-export default App
+  
+)
+}}
