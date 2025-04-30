2532b80ecd87d66e786524c99c9c4020fbedf89a
FigueroaLucas <uuuuiikii@gmail.com>
2025-04-29 23:56:31 -0300

    Ejercicio adicional Tp3 FigueroaLucas y VelazquezMatias

---
+++ src/productos.js
@@ -0,0 +1,7 @@
+export const productos = [
+    { id: 1, descripcion: "Teclado", precio: 30000 },
+    { id: 2, descripcion: "Auriculares", precio: 49000 },
+    { id: 3, descripcion: "Monitor", precio: 120000 },
+    { id: 4, descripcion: "Mouse", precio: 15000 },
+    { id: 5, descripcion: "Gabinete", precio: 35000 }
+  ];
